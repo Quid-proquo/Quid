06a1472f9911b05c8906d03565684a3477362e5e
Josue19-08 <josuemarin2009@hotmail.com>
2026-03-28 11:59:10 -0600

    feat(backend): scaffold missions module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MissionsModule } from './missions/missions.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
@@ -15,6 +16,7 @@ import { PrismaModule } from './prisma/prisma.module';
     ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
+    MissionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
